32bebf679edddc87830cd41d686ad930231ff6c1
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-01-16 22:05:26 +0200

    Login Response Dto

---
+++ InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/dto/response/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.rss.InventoryManagementSystemUserService.dto.response;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class LoginResponse {
+
+    private String token;
+
+    private String role;
+}
