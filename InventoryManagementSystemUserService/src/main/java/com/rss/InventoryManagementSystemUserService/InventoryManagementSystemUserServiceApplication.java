989973d8b39ef8721d694e01a85914aecc4c7ebf
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-01-16 21:12:32 +0200

    User Service Initialization

---
+++ InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/InventoryManagementSystemUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rss.InventoryManagementSystemUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemUserServiceApplication.class, args);
+	}
+
+}
---
+++ InventoryManagementSystemUserService/src/test/java/com/rss/InventoryManagementSystemUserService/InventoryManagementSystemUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rss.InventoryManagementSystemUserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
