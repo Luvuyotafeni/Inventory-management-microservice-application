d07e2eaca098997a66adf66dcbc672651efe7b41
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-01-17 22:26:30 +0200

    auth controller for login functionality

---
+++ InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.rss.InventoryManagementSystemUserService.controller;
+
+
+import com.rss.InventoryManagementSystemUserService.dto.request.LoginRequest;
+import com.rss.InventoryManagementSystemUserService.dto.response.LoginResponse;
+import com.rss.InventoryManagementSystemUserService.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public LoginResponse login(@Valid @RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+}
