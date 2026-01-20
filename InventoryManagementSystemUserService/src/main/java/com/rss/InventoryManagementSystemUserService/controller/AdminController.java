7f1b0ae76187ee2951d8a4a9bac2c79c25687236
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-01-20 08:54:32 +0200

    get all users controller

--- InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/controller/AdminController.java
+++ InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/controller/AdminController.java
@@ -1,4 +1,25 @@
 package com.rss.InventoryManagementSystemUserService.controller;
 
+import com.rss.InventoryManagementSystemUserService.dto.response.AdminUserResponse;
+import com.rss.InventoryManagementSystemUserService.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ADMIN','SUPER_ADMIN)")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/users")
+    public List<AdminUserResponse> getAllUsers(){
+        return adminService.getAllUsers();
+    }
 }
