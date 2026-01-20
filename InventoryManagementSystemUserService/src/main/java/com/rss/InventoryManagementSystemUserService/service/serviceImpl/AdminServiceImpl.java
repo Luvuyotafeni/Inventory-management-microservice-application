d3abbb3c558739edc581a3d9aea6b701336494da
Luvuyo Tafeni <luvuyotafeni012@gmail.com>
2026-01-20 08:43:50 +0200

    Admin services for changing role, activate user, approve user, suspend user logic added successfully

--- InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/service/serviceImpl/AdminServiceImpl.java
+++ InventoryManagementSystemUserService/src/main/java/com/rss/InventoryManagementSystemUserService/service/serviceImpl/AdminServiceImpl.java
@@ -3,6 +3,7 @@ package com.rss.InventoryManagementSystemUserService.service.serviceImpl;
 import com.rss.InventoryManagementSystemUserService.Enum.Role;
 import com.rss.InventoryManagementSystemUserService.Enum.UserStatus;
 import com.rss.InventoryManagementSystemUserService.dto.response.AdminUserResponse;
+import com.rss.InventoryManagementSystemUserService.entity.User;
 import com.rss.InventoryManagementSystemUserService.repository.UserRepository;
 import com.rss.InventoryManagementSystemUserService.service.AdminService;
 import jakarta.transaction.Transactional;
@@ -35,21 +36,46 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void approveUser(Long userId) {
+        User user = getUser(userId);
 
+        if (user.getStatus() == UserStatus.SUSPENDED) {
+            throw new IllegalStateException("Suspended users must be activated first");
+        }
+
+        user.setStatus(UserStatus.APPROVED);
     }
 
     @Override
     public void suspendUser(Long userId) {
-
+        User user = getUser(userId);
+        user.setStatus(UserStatus.SUSPENDED);
     }
 
     @Override
     public void activateUser(Long userId) {
+        User user = getUser(userId);
 
+        if (user.getStatus() != UserStatus.SUSPENDED) {
+            throw new IllegalStateException("Only suspended users can be activated");
+        }
+
+        user.setStatus(UserStatus.APPROVED);
     }
 
     @Override
     public void changeRole(Long userId, Role role) {
+        User user = getUser(userId);
 
+        if (user.getStatus() == UserStatus.SUSPENDED) {
+            throw new IllegalStateException("Cannot change role of suspended user");
+        }
+
+        user.setRole(role);
+    }
+
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found with id: " + userId));
     }
 }
